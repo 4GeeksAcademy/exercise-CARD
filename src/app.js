82ebcbeffe95d23726699a5978d05c6aa7835c51
PixelJotape <gabopporrino@gmail.com>
2026-04-09 00:39:33 +0000

    Generador de Cartas TERMINADO

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let icons = ["♦", "♥", "♠", "♣"];
+  let values = [`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`J`,`Q`,`K`,`A`,];
+
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)];
+  let randomValue = values[Math.floor(Math.random() * values.length)]; 
+
+  document.getElementById('top-icon').innerHTML = randomIcon;
+  document.getElementById('mid-text').innerHTML = randomValue;
+  document.getElementById(`adc-icon`).innerHTML = randomIcon;
+
+  let topElement = document.getElementById('top-icon');
+  let midElement = document.getElementById('mid-text');
+  let adcElement = document.getElementById(`adc-icon`);
+
+  if (randomIcon == '♥' || randomIcon == '♦') {
+    topElement.style.color = 'red';
+    midElement.style.color = `red`;
+    adcElement.style.color = 'red';
+  }
+  else{
+    topElement.style.color = 'black';
+    midElement.style.color = 'black';
+    adcElement.style.color = 'black';
+  }
 };
